b9432b0a15a205b881c90c69d9e4fd4097ddd508
lepyavko.d <lepyavko.d@novaposhta.ua>
2022-11-07 14:00:18 +0200

    spinWords kata

--- katas/spinWords/spinWords.ts
+++ katas/spinWords/spinWords.ts
@@ -1,3 +1,9 @@
+function reverseWord(word: string): string {
+  return word.split('').reverse().join('');
+}
+
 export function spinWords(words: string): string {
-  return words;
+  const arr = words.split(' ');
+  const spinnedWords = arr.map((word) => (word.length >= 5 ? reverseWord(word) : word));
+  return spinnedWords.join(' ');
 }
