1236064e124550fac339a1bdb88d32c8e5224be8
lepyavko.d <lepyavko.d@novaposhta.ua>
2022-11-07 16:58:36 +0200

    dashatize kata

--- katas/dashatize/dashatize.ts
+++ katas/dashatize/dashatize.ts
@@ -1,3 +1,26 @@
+function isOdd(num: number): boolean {
+  return Boolean(num % 2);
+}
+
 export function dashatize(num: number): string {
-  return 'NaN';
+  if (!Number.isInteger(num) || num === 0 || (num < 0 && String(num).length === 2)) {
+    return 'NaN';
+  }
+  const arr = String(num).split('');
+  if (arr[0] === '-') {
+    delete arr[0];
+  }
+  for (let i = 0; i <= arr.length; i += 1) {
+    const val = Math.abs(Number(arr[i]));
+    if (isOdd(val)) {
+      if (i === 0) {
+        arr[i] = `${val}-`;
+      } else if (i === arr.length - 1) {
+        arr[i] = `-${val}`;
+      } else {
+        arr[i] = `-${val}-`;
+      }
+    }
+  }
+  return `${arr.join('').replaceAll('--', '-')}`;
 }
