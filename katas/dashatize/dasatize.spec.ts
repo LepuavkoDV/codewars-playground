45030d774427de4b758f19497179129be79ea59f
lepyavko.d <lepyavko.d@novaposhta.ua>
2022-11-07 16:26:57 +0200

    dashatize tests

---
+++ katas/dashatize/dasatize.spec.ts
@@ -0,0 +1,18 @@
+import { dashatize } from './dashatize';
+
+describe('dashatize', () => {
+  it(`If n is an integer, Return a string with dash "-" marks before and after each odd integer,
+  but do not begin or end the string with a dash mark.
+  If n is negative, then the negative sign should be removed.`, () => {
+    expect(dashatize(274)).toBe('2-7-4');
+    expect(dashatize(5311)).toBe('5-3-1-1');
+    expect(dashatize(86320)).toBe('86-3-20');
+    expect(dashatize(974302)).toBe('9-7-4-3-02');
+    expect(dashatize(-28369)).toBe('28-3-6-9');
+  });
+  it('If n is not an integer, return the string "NaN".', () => {
+    expect(dashatize(NaN)).toBe('NaN');
+    expect(dashatize(0)).toBe('NaN');
+    expect(dashatize(-1)).toBe('NaN');
+  });
+});
---
+++ katas/dashatize/dashatize.ts
@@ -0,0 +1,3 @@
+export function dashatize(num: number): string {
+  return 'NaN';
+}
